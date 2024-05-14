9ca2ac5fcdc00391ff1b4df2f73b36939e5e9c30
Josh Duffek <joshduffek@joshd.local>
2024-05-14 12:32:28 -0500

    tried SNMP in to rest

--- jnms.py
+++ jnms.py
@@ -10,11 +10,14 @@ def main():
     # Execute service discovery script
     subprocess.run(["python3", "svc-disco.py"])
 
-    # Execute SNMP script...soon.
-    #subprocess.run(["python3", "snmp.py"])
+    # Start SNMP script as a background process
+    snmp_process = subprocess.Popen(["python3", "snmp.py"])
 
     # Execute service discovery script
     subprocess.run(["python3", "monitors.py"])
+
+    # Wait for the SNMP script to finish before exiting (optional)
+    snmp_process.wait()
 
 if __name__ == "__main__":
     main()
